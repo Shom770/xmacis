22cdab6995960539b800a42515532ff8273ccfca
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-01-21 23:44:06 -0500

    add getting station data with the Data class

--- historical_weather/_wrapped_endpoints.py
+++ historical_weather/_wrapped_endpoints.py
@@ -1,3 +1,4 @@
+from ast import literal_eval
 from datetime import datetime
 import typing
 
@@ -11,6 +12,17 @@ _SESSION = requests.Session()
 class Data:
     """Contains the data retrieved from the xmacis API."""
 
+    def __init__(self, **kwargs):
+        for element, value in kwargs.items():
+            setattr(self, element.lower(), literal_eval(value))
+
+    def __repr__(self):
+        represent_instance = "Data("
+
+        for attr_name, attr_value in vars(self).items():
+            represent_instance += f"{attr_name}={attr_value}, "
+
+        return represent_instance.removesuffix(", ") + ")"
 
 def get_station_data(
     station_id: str,
@@ -21,14 +33,12 @@ def get_station_data(
     date: typing.Optional[datetime] = None,
 ) -> Data:
     """Retrieves the station data based off the filters provided."""
-
     parameters = {param_name: param_value.strftime("%Y-%m-%d") if isinstance(param_value, datetime) else param_value
                   for param_name, param_value in zip(
         ("sid", "sdate", "edate", "date", "elems"),
-        (station_id, start_date, end_date, date, elements)
+        (station_id, start_date, end_date, date, ",".join(element.value for element in elements))
     ) if param_value}
 
     response = _SESSION.get("http://data.rcc-acis.org/StnData", params=parameters).json()
-    print(response)
 
-get_station_data("DCA", [__import__("elements").SNOW], date=datetime(2022, 1, 3))
+get_station_data("DCA", [__import__("elements").Elements.SNOW, __import__("elements").Elements.MAXIMUM_TEMPERATURE], date=datetime(2022, 1, 3))
--- historical_weather/elements.py
+++ historical_weather/elements.py
@@ -1,10 +1,14 @@
-MAXIMUM_TEMPERATURE = "maxt"
-MINIMUM_TEMPERATURE = "mint"
-AVERAGE_TEMPERATURE = "avgt"
-OBSERVED_TEMPERATURE = "obst"
-PRECIPITATION = "pcpn"
-SNOW = "snow"
-SNOW_DEPTH = "snwd"
-COOLING_DEG_DAYS = "cdd"
-DAYS_BELOW_BASE = "hdd"
-DAYS_ABOVE_BASE = "gdd"
+from enum import Enum
+
+
+class Elements(Enum):
+    MAXIMUM_TEMPERATURE = "maxt"
+    MINIMUM_TEMPERATURE = "mint"
+    AVERAGE_TEMPERATURE = "avgt"
+    OBSERVED_TEMPERATURE = "obst"
+    PRECIPITATION = "pcpn"
+    SNOW = "snow"
+    SNOW_DEPTH = "snwd"
+    COOLING_DEG_DAYS = "cdd"
+    DAYS_BELOW_BASE = "hdd"
+    DAYS_ABOVE_BASE = "gdd"
