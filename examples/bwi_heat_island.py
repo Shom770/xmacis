52fc08a6827c25980cc17348cd0557df3f45fe2d
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-01-22 18:56:12 -0500

    create /examples

---
+++ examples/bwi_heat_island.py
@@ -0,0 +1,19 @@
+"""An analysis comparing daily temperatures between IAD and BWI to see the effect of BWI's 'heat island'."""
+
+from datetime import datetime
+
+from ..historical_weather import Elements, get_station_data
+
+dp_iad = get_station_data(
+    "IAD",
+    [Elements.MINIMUM_TEMPERATURE, Elements.MAXIMUM_TEMPERATURE],
+    start_date=datetime(year=2020, month=2, day=20),  # The start date is Feb. 20th, 2020 because that is when BWI became a 'heat island' apparently.
+    end_date=datetime.today()
+)
+
+dp_bwi = get_station_data(
+    "BWI",
+    [Elements.MINIMUM_TEMPERATURE, Elements.MAXIMUM_TEMPERATURE],
+    start_date=datetime(year=2020, month=2, day=20),
+    end_date=datetime.today()
+)
