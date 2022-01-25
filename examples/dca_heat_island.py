4cc8de85d1d43536ba23eb69ec199c470ac780d6
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-01-25 16:21:53 -0500

    DCA heat island

--- examples/dca_heat_island.py
+++ examples/dca_heat_island.py
@@ -16,8 +16,8 @@ dp_iad = get_station_data(
     end_date=datetime.today() - timedelta(days=1)
 )
 
-dp_bwi = get_station_data(
-    "BWI",
+dp_dca = get_station_data(
+    "DCA",
     [Elements.MINIMUM_TEMPERATURE, Elements.MAXIMUM_TEMPERATURE],
     start_date=datetime(year=2016, month=1, day=1),
     end_date=datetime.today() - timedelta(days=1)
@@ -25,15 +25,15 @@ dp_bwi = get_station_data(
 
 
 def heat_island_effects() -> dict:
-    """Compares IAD max/min temperature to BWI's and figures out the mean/median of BWI's inaccuracies."""
+    """Compares IAD max/min temperature to DCA's and figures out the mean/median of DCA's inaccuracies."""
     differences = defaultdict(list)
 
-    for day, (iad_data, bwi_data) in zip(
-            dp_iad.data_points.keys(), zip(dp_iad.data_points.values(), dp_bwi.data_points.values())
+    for day, (iad_data, dca_data) in zip(
+            dp_iad.data_points.keys(), zip(dp_iad.data_points.values(), dp_dca.data_points.values())
     ):
         differences[day.strftime("%B %Y")].append((
-            (bwi_data.minimum_temperature - iad_data.minimum_temperature)
-            + (bwi_data.maximum_temperature - iad_data.maximum_temperature)
+            (dca_data.minimum_temperature - iad_data.minimum_temperature)
+            + (dca_data.maximum_temperature - iad_data.maximum_temperature)
         ) / 2)
 
     return {key: sum(value) / len(value) for key, value in differences.items()}
@@ -47,9 +47,6 @@ month_labels = np.array(list(differences.keys()))
 plt.title("DCA's difference from IAD in temperatures monthly on average since January 1st, 2016")
 
 plt.xticks([tick for tick in range(len(month_labels))], month_labels, rotation=90)
-
-plt.axvline(x=48)  # since BWI became a 'heat island'
-plt.axhline(y=0)  # demonstrate how no month average has reached below IAD on average
 
 plt.plot(month_labels, departure, color="red")
 
