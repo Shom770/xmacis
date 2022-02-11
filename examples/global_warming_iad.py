ee31c85ab8cff2c46f3652e63d5d61d280a93157
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-02-11 16:19:33 -0500

    initial commit for global warming affects at IAD

---
+++ examples/global_warming_iad.py
@@ -0,0 +1,27 @@
+"""An analysis comparing IAD's average monthly high temperature during summer from 1950 to now."""
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+from historical_weather import Elements, get_station_data
+import matplotlib.pyplot as plt
+import numpy as np
+
+dp_iad = get_station_data(
+    "IAD",
+    [Elements.MAXIMUM_TEMPERATURE],
+    start_date=datetime(year=1950, month=1, day=1),
+    end_date=datetime.today() - timedelta(days=1)
+)
+
+
+def global_warming_effects() -> dict:
+    """Gets IAD's average monthly high temperature from 1950 till now."""
+    years_and_avg = defaultdict(list)
+
+    for period, data in dp_iad.data_points.items():
+        if not datetime(year=period.year, month=6, day=22) <= period < datetime(year=period.year, month=9, day=23):
+            continue
+
+        years_and_avg[period.year].append(data.maximum_temperature)
+
+    return {key: sum(value) / len(value) for key, value in years_and_avg.items()}
